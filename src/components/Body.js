1f2e4e12e65d853573ede9eb44a37f5e3c6b2c2b
ananthmgk <ananthmgk4002@gmail.com>
2024-01-05 03:38:09 +0530

    added category 3 layer

--- src/components/Body.js
+++ src/components/Body.js
@@ -68,7 +68,7 @@ const Body = () => {
           <button className="dropbtn p-2 m-2 px-8 mx-10 bg-yellow-700 hover:bg-yellow-500 text-white rounded-md">
             Category
           </button>
-          <div className="dropdown-content absolute hidden bg-amber-100 min-w-40 shadow-lg">
+          <div className="dropdown-content1 absolute left-0 hidden bg-amber-100 w-40 shadow-lg">
             <Link to="/" className="block text-black px-3 py-4">
               Level 1a
             </Link>
@@ -80,6 +80,34 @@ const Body = () => {
             </Link>
             <Link to="/" className="block text-black px-3 py-4">
               Level 1d
+            </Link>
+          </div>
+          <div className="dropdown-content2 absolute hidden bg-amber-100 w-40 ml-40 shadow-lg">
+            <Link to="/" className="block text-black px-3 py-4">
+              Level 2a
+            </Link>
+            <Link to="/" className="block text-black px-3 py-4">
+              Level 2b
+            </Link>
+            <Link to="/" className="block text-black px-3 py-4">
+              Level 2c
+            </Link>
+            <Link to="/" className="block text-black px-3 py-4">
+              Level 2d
+            </Link>
+          </div>
+          <div className="dropdown-content3 absolute hidden bg-amber-100 w-40 ml-80 shadow-lg">
+            <Link to="/" className="block text-black px-3 py-4">
+              Level 3a
+            </Link>
+            <Link to="/" className="block text-black px-3 py-4">
+              Level 3b
+            </Link>
+            <Link to="/" className="block text-black px-3 py-4">
+              Level 3c
+            </Link>
+            <Link to="/" className="block text-black px-3 py-4">
+              Level 3d
             </Link>
           </div>
         </div>

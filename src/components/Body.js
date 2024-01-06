5644c8225a0cff016c0a8e59822b347cc672fb32
ananthmgk <ananthmgk4002@gmail.com>
2024-01-06 22:15:20 +0530

    replaced hover in css used if condition

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,6 +8,13 @@ const Body = () => {
   const [allProducts, setallProducts] = useState(productList);
   const [filteredProducts, setfilteredProducts] = useState(productList);
   const [searchInput, setSearchInput] = useState("");
+  const [showCategoryList, setShowCategoryList] = useState(false);
+  const [showCarList, setShowCarList] = useState(false);
+  const [showBikeList, setShowBikeList] = useState(false);
+  const [showHyundaiList, setShowHyundaiList] = useState(false);
+
+  // const [showBiscuitList, setShowBiscuitList] = useState(false);
+  // const [showSnacksList, setShowSnacksList] = useState(false);
 
   const searchedData = () => {
     const data = filterData(searchInput, allProducts);
@@ -25,6 +32,13 @@ const Body = () => {
   }
 
   const starterDebounce = debounceing(searchedData);
+
+  const deleteAll1 = () => {
+    showCategoryList && setShowBikeList(false);
+  };
+
+  const deleteAll = () =>
+    showCategoryList && showBikeList ? setShowCategoryList(false) : null;
 
   // useEffect(() => {
   //   getProducts();
@@ -65,51 +79,129 @@ const Body = () => {
           Search
         </button>
         <div className="dropdown justify-items-center items-center relative inline-block">
-          <button className="dropbtn p-2 m-2 px-8 mx-10 bg-yellow-700 hover:bg-yellow-500 text-white rounded-md">
+          <button
+            className="dropbtn p-2 m-2 px-8 mx-10 bg-yellow-700 hover:bg-yellow-500 text-white rounded-md"
+            onClick={() => {
+              !showCategoryList
+                ? setShowCategoryList(true)
+                : setShowCategoryList(false);
+              setShowCarList(false);
+              setShowBikeList(false);
+            }}
+          >
             Category
           </button>
-          <div className="dropdown-content1 absolute left-0 hidden bg-amber-100 w-40 shadow-lg">
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 1a
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 1b
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 1c
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 1d
-            </Link>
-          </div>
-          <div className="dropdown-content2 absolute hidden bg-amber-100 w-40 ml-40 shadow-lg">
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 2a
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 2b
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 2c
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 2d
-            </Link>
-          </div>
-          <div className="dropdown-content3 absolute hidden bg-amber-100 w-40 ml-80 shadow-lg">
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 3a
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 3b
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 3c
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 3d
-            </Link>
-          </div>
+
+          {showCategoryList && (
+            <div className="category-list absolute left-0 bg-amber-100 w-40 shadow-lg">
+              <Link
+                to="/"
+                className="car block hover:bg-yellow-500 text-black px-3 py-4"
+                onMouseEnter={() => setShowCarList(true)}
+                onMouseLeave={() => {
+                  showBikeList ? setShowCarList(false) : null;
+                }}
+              >
+                Car
+              </Link>
+              <Link
+                to="/"
+                className="bike block hover:bg-yellow-500 text-black px-3 py-4"
+                onMouseEnter={() => setShowBikeList(true)}
+                onMouseLeave={() => {
+                  showCarList ? setShowBikeList(false) : null;
+                }}
+              >
+                Bike
+              </Link>
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                Biscuit
+              </Link>
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                Snacks
+              </Link>
+            </div>
+          )}
+          {showCarList && (
+            <div className="car-list absolute bg-amber-100 w-40 ml-40 shadow-lg">
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+                onMouseEnter={() => setShowHyundaiList(true)}
+                onMouseLeave={() => setShowHyundaiList(false)}
+              >
+                Hyundai
+              </Link>
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                Honda
+              </Link>
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                Maruti
+              </Link>
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                Skoda
+              </Link>
+            </div>
+          )}
+          {showBikeList && (
+            <div className="bike-list absolute bg-amber-100 w-40 ml-40 shadow-lg">
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                TVS
+              </Link>
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                Bajaj
+              </Link>
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                Honda
+              </Link>
+              <Link
+                to="/"
+                className="block hover:bg-yellow-500 text-black px-3 py-4"
+              >
+                Yamaha
+              </Link>
+            </div>
+          )}
+          {showHyundaiList && (
+            <div className="hyundai-model absolute bg-amber-100 w-40 ml-80 shadow-lg">
+              <Link to="/" className="block text-black px-3 py-4">
+                Santro
+              </Link>
+              <Link to="/" className="block text-black px-3 py-4">
+                I-10
+              </Link>
+              <Link to="/" className="block text-black px-3 py-4">
+                Verna
+              </Link>
+              <Link to="/" className="block text-black px-3 py-4">
+                I-20
+              </Link>
+            </div>
+          )}
         </div>
       </div>
       <div className="flex flex-wrap ">

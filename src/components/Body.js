0bd97f7607d0066795b252bc8f5b0866a7c82444
ananthmgk <ananthmgk4002@gmail.com>
2024-01-12 15:22:07 +0530

    included data from json

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,17 +3,42 @@ import BasketCard from "./BasketCard";
 import { useState } from "react";
 import { filterData } from "../uttilites/helper";
 import { Link } from "react-router-dom";
+import { categoriesList } from "../config";
+import { useRef } from "react";
+
+// ---------------------------------------------------------
+
+// ---------------------------------------------------
 
 const Body = () => {
   const [allProducts, setallProducts] = useState(productList);
   const [filteredProducts, setfilteredProducts] = useState(productList);
   const [searchInput, setSearchInput] = useState("");
-  const [showCategoryList, setShowCategoryList] = useState(false);
-  const [showCarList, setShowCarList] = useState(false);
-  const [showBikeList, setShowBikeList] = useState(false);
-  const [showBiscuitList, setShowBiscuitList] = useState(false);
-  const [showAppliancesList, setShowAppliancesList] = useState(false);
-  const [showHyundaiList, setShowHyundaiList] = useState(false);
+  const [categories, setCategories] = useState(categoriesList);
+  //
+  const categoryRef = useRef(null);
+  const firstChildrenRef = useRef(null);
+  const secondChildrenRef = useRef(null);
+  //
+  const [showChildren, setShowChildren] = useState(false);
+  const [childrenList, setChildrenList] = useState([]);
+  //
+  const handleMouseEnter = (children) => {
+    setChildrenList(children);
+  };
+
+  const handleMouseLeave = () => {
+    setShowChildren(false);
+    setChildrenList([]);
+  };
+
+  // const [showCategoryList, setShowCategoryList] = useState(false);
+  // const [showCarList, setShowCarList] = useState(false);
+  // const [showBikeList, setShowBikeList] = useState(false);
+  // const [showBiscuitList, setShowBiscuitList] = useState(false);
+  // const [showAppliancesList, setShowAppliancesList] = useState(false);
+  // const [showHyundaiList, setShowHyundaiList] = useState(false);
+  // ------------------------------------------
 
   const searchedData = () => {
     const data = filterData(searchInput, allProducts);
@@ -31,13 +56,6 @@ const Body = () => {
   }
 
   const starterDebounce = debounceing(searchedData);
-
-  const deleteAll1 = () => {
-    showCategoryList && setShowBikeList(false);
-  };
-
-  const deleteAll = () =>
-    showCategoryList && showBikeList ? setShowCategoryList(false) : null;
 
   // useEffect(() => {
   //   getProducts();
@@ -81,197 +99,48 @@ const Body = () => {
           <button
             className="dropbtn p-2 m-2 px-8 mx-10 bg-yellow-700 hover:bg-yellow-500 text-white rounded-md"
             onClick={() => {
-              !showCategoryList
-                ? setShowCategoryList(true)
-                : setShowCategoryList(false);
-              setShowCarList(false);
-              setShowBikeList(false);
+              categoryRef.current.style.display = "block";
             }}
           >
             Category
           </button>
 
-          {showCategoryList && (
-            <div className="category-list absolute left-0 bg-amber-100 w-40 shadow-lg">
-              <Link
-                to="/"
-                className="car block hover:bg-yellow-500 text-black px-3 py-4"
-                onMouseEnter={() => {
-                  setShowCarList(true);
-                  setShowBikeList(false);
-                  setShowBiscuitList(false);
-                  setShowAppliancesList(false);
-                }}
-                // onMouseLeave={() => {
-                //   !showCarList ? setShowCarList(false) : setShowCarList(true);
-                // }}
-              >
-                Cars
-              </Link>
-              <Link
-                to="/"
-                className="bike block hover:bg-yellow-500 text-black px-3 py-4"
-                onMouseEnter={() => {
-                  setShowBikeList(true);
-                  setShowCarList(false);
-                  setShowBiscuitList(false);
-                  setShowAppliancesList(false);
-                }}
-                // onMouseLeave={() => setShowBikeList(false)}
-              >
-                Bikes
-              </Link>
-              <Link
-                to="/"
-                className="biscuit block hover:bg-yellow-500 text-black px-3 py-4"
-                onMouseEnter={() => {
-                  setShowBiscuitList(true);
-                  setShowBikeList(false);
-                  setShowAppliancesList(false);
-                }}
-              >
-                Biscuits
-              </Link>
-              <Link
-                to="/"
-                className="home-appliances block hover:bg-yellow-500 text-black px-3 py-4"
-                onMouseEnter={() => {
-                  setShowAppliancesList(true);
-                  setShowBiscuitList(false);
-                }}
-              >
-                Home Appliances
-              </Link>
-            </div>
-          )}
-          {showCarList && (
-            <div className="car-list absolute bg-amber-100 w-40 ml-40 shadow-lg">
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Hyundai
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Honda
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Maruti
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Skoda
-              </Link>
-            </div>
-          )}
-          {showBikeList && (
-            <div className="bike-list absolute bg-amber-100 w-40 ml-40 shadow-lg">
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                TVS
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Bajaj
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Honda
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Yamaha
-              </Link>
-            </div>
-          )}
-          {showBiscuitList && (
-            <div className="biscuit-list absolute bg-amber-100 w-40 ml-40 shadow-lg">
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Good Day
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Parle G
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Britannia
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Sunfeast
-              </Link>
-            </div>
-          )}
-          {showAppliancesList && (
-            <div className="appliance-list absolute bg-amber-100 w-40 ml-40 shadow-lg">
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                TV
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Refrigerator
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Washing Machine
-              </Link>
-              <Link
-                to="/"
-                className="block hover:bg-yellow-500 text-black px-3 py-4"
-              >
-                Air Conditioner
-              </Link>
-            </div>
-          )}
-          {showHyundaiList && (
-            <div className="hyundai-model absolute bg-amber-100 w-40 ml-80 shadow-lg">
-              <Link to="/" className="block text-black px-3 py-4">
-                Santro
-              </Link>
-              <Link to="/" className="block text-black px-3 py-4">
-                I-10
-              </Link>
-              <Link to="/" className="block text-black px-3 py-4">
-                Verna
-              </Link>
-              <Link to="/" className="block text-black px-3 py-4">
-                I-20v
-              </Link>
-            </div>
-          )}
+          <div
+            className="category-list absolute left-0 bg-amber-100 w-40 shadow-lg"
+            ref={categoryRef}
+          >
+            {categories.map((category) => {
+              return (
+                <Link
+                  className="block hover:bg-yellow-500 text-black px-3 py-4"
+                  key={category.id}
+                  onMouseEnter={() => {
+                    handleMouseEnter(category.children);
+                    firstChildrenRef.current.style.display = "block";
+                  }}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  {category.name}
+                </Link>
+              );
+            })}
+          </div>
+          <div
+            className="firstchildren-list absolute bg-amber-100 w-40 ml-40 shadow-lg"
+            ref={firstChildrenRef}
+          >
+            {childrenList.map((childL1) => {
+              return (
+                <Link
+                  to="/"
+                  className="block hover:bg-yellow-500 text-black px-3 py-4"
+                  key={childL1.id}
+                >
+                  {childL1.name}
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
       <div className="flex flex-wrap ">
